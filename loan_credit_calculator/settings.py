dd9e6f27cacf5f835efdaf5bf7df34c1c4eca5fa
Emily Nieves <nemily1710@gmail.com>
2025-05-15 13:52:43 -0500

    fix: :ambulance: fix error

--- loan_credit_calculator/settings.py
+++ loan_credit_calculator/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'loan_credit_calculator.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default=os.environ.get("RENDER_DATABASE_URL"),
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )

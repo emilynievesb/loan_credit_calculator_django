d5710900b4e6ef951b9d0a5566ff157ac17a107d
Emily Nieves <nemily1710@gmail.com>
2025-05-15 14:35:47 -0500

    fix: :ambulance: fix db errors

--- calculator/models.py
+++ calculator/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class UserProfile(AbstractUser):
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=20, unique=True)
-    password = models.CharField(max_length=20)
+    email = models.EmailField(max_length=150,unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=254)
 
     def __str__(self):
         return self.username

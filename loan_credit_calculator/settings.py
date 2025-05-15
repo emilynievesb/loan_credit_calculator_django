a9958aeaa4c2a358a3f5d41cacc4c29ad896ac80
Emily Nieves <nemily1710@gmail.com>
2025-05-15 13:50:11 -0500

    fix: :ambulance: fix deploy errors

--- loan_credit_calculator/settings.py
+++ loan_credit_calculator/settings.py
@@ -83,8 +83,9 @@ WSGI_APPLICATION = 'loan_credit_calculator.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
+        default=os.environ.get("RENDER_DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 

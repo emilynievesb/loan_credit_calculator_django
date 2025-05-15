17f3047ea344b27ed8ac983c145c9c966bfe135c
Emily Nieves <nemily1710@gmail.com>
2025-05-15 14:15:45 -0500

    fix: :ambulance: fix error

--- loan_credit_calculator/settings.py
+++ loan_credit_calculator/settings.py
@@ -11,12 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
